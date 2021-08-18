7ba03f279e49a5e7aaedc60f7a3fb9db52928715
Inon Bercu <inonbr@gmail.com>
2021-08-18 09:08:33 +0300

    started react app

---
+++ li_front_end/src/App.js
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Button variant='primary'>Primary</Button>
+      <h1>test me</h1>
+    </div>
+  );
+}
+
+export default App;
